1b4711ca6f0a80ead92f2bff2a19b5765645d239
Sudhakar Sharma <117532270+sudhakarsharma07@users.noreply.github.com>
2025-05-18 20:04:40 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// ✅ Add the base path here
 export default defineConfig({
-  plugins: [react()], // Replace with your repository name
+  plugins: [react()], 
 });
