40ccbc72a62848ae84cf3695d4878a2a2aa96753
Sudhakar Sharma <117532270+sudhakarsharma07@users.noreply.github.com>
2025-05-18 19:19:07 +0530

    fix:fix the config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react';
 
 // ✅ Add the base path here
 export default defineConfig({
-  plugins: [react()],
-  base: '/finance-dashboard/' // Replace with your repository name
+  plugins: [react()], // Replace with your repository name
 });
